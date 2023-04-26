9abccc9c703a578d55e210a17a5b78a6be2c7d05
Levi Bostian <levi@levi.earth>
2023-04-26 06:36:51 -0500

    add workflow

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
-export { parse as flagsParse } from "https://deno.land/std@0.184.0/flags/mod.ts";
-export * as logger from "https://deno.land/std@0.184.0/log/mod.ts";
-import ical from "npm:ical-generator@4.1.0-develop.1"; export {ical};
-export type {ICalEventData} from "npm:ical-generator@4.1.0-develop.1"
-export { Octokit } from "npm:@octokit/rest@19.0.7"
-export {posix as paths, posix} from "https://deno.land/std@0.184.0/path/mod.ts";
+export { parse as flagsParse } from "https://deno.land/std@0.184.0/flags/mod.ts#^";
+export * as logger from "https://deno.land/std@0.184.0/log/mod.ts#^";
+import ical from "npm:ical-generator@4.0.0#^"; 
+export {ical};
+export type {ICalEventData} from "npm:ical-generator@4.0.0#^"
+export { Octokit } from "npm:@octokit/rest@19.0.7#^"
+export {posix as paths, posix} from "https://deno.land/std@0.184.0/path/mod.ts#^";
